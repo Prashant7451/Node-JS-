efd686b0da4ed50ef192311381d9810519b74169
Prashant7451 <103504965+Prashant7451@users.noreply.github.com>
2023-01-04 21:40:48 +0530

    some changes

---
+++ app.js
@@ -0,0 +1,11 @@
+//npm - global command,comes with node 
+//npm --version
+//local dependency - use it only in this particular project
+//npm i -g  <packageName>
+//sudo install -g <packageName> (mac)
+
+//package.json - manifest file (stores important info about project /package)'
+//manual approach (create package.json in the root ,create properties etc.)
+//npm init (step by step ,oress enter to skip)
+//npm init -y(everything default)
+  
